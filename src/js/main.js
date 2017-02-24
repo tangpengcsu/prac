b9b30551f5b42d6cc178c1e525fd9c40d99b2c5c
唐鹏 <tang.peng@hotmail.com>
2017-02-24 14:31:21 +0800

    修改

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
-(function(){
-   $
-});
+(function () {
+    console.log("start------------")
+    function SuperType(){
+        this.property = true;
+    }
+    SuperType.prototype.getSuperValue = function(){
+        return this.property;
+    };
+    function SubType(){
+        this.subproperty = false;
+    }
+    SubType.prototype = new SuperType();
+
+    SubType.prototype.getSubValue = function(){
+        return this.subproperty;
+    };
+    var instance = new SubType();
+    console.log(instance.getSuperValue());
+    console.log(instance.getSubValue());
+
+    console.log(instance instanceof Object);
+    console.log(instance instanceof SuperType)
+    console.log(instance instanceof SubType)
+
+    console.log(Object.prototype.isPrototypeOf(instance))
+    console.log(SubType.prototype.isPrototypeOf(instance))
+    console.log(SuperType.prototype.isPrototypeOf(instance))
+
+    console.log("end-------------")
+}());
